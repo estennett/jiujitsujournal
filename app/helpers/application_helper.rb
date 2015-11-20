3ea813d1896b2d387be9acc0b9c80e8e378055e6
Eric Stennett <estennett@gmail.com>
2015-11-20 09:14:20 -0500

    last commit before making video

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def embed(video_url)
+    youtube_id = video_url.split("=").last
+    content_tag(:iframe, nil, src: "//www.youtube.com/embed/#{youtube_id}")
+  end
+
 end
